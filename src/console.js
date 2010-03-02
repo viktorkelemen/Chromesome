156f1a90394441ab8c514d4d08c7d877383ee45f
Viktor Kelemen <viktor@cirius.co.jp>
2010-03-03 01:13:44 +0900

    Refactoring, adding doc

--- src/console.js
+++ src/console.js
@@ -7,8 +7,7 @@
 var chromesomeInit = function () {
 
 
-    var dev = {},
-        current = undefined;
+    var current = undefined;
 
     /**
     * Changes the current node to the first childnode that matches the nodeDescription
@@ -16,7 +15,7 @@ var chromesomeInit = function () {
     * @param nodeDescription {String}
     * @return HtmlNode
     */
-    function cd (nodeDescription) {
+    function cd(nodeDescription) {
 
         var selected;
 
@@ -49,8 +48,8 @@ var chromesomeInit = function () {
     *
     * @return {Array}
     */
-    function ls () {
-        
+    function ls() {
+
         var result = [],
             l = 0,
             txt = "",
@@ -82,114 +81,153 @@ var chromesomeInit = function () {
         }
     }
 
-        dev.cat = function (element) {
+    /**
+    * Shows the content of a childnode
+    *
+    * @param nodeDescription {String}
+    * @return {String}
+    */
+    function cat(nodeDescription) {
 
-            // not implemented yet
-            // var selected;
-            //
-            // if (current !== undefined) {
-            //     selected = $(element)[0];
-            //     if (selected !== undefined) {
-            //         return ($(selected).text());
-            //     } else {
-            //         return element + " not found.";
-            //     }
-            // }
-            return "Not implemented yet";
-        };
+        // not implemented yet
+        // var selected;
+        //
+        // if (current !== undefined) {
+        //     selected = $(element)[0];
+        //     if (selected !== undefined) {
+        //         return ($(selected).text());
+        //     } else {
+        //         return element + " not found.";
+        //     }
+        // }
+        return "Not implemented yet";
+    }
 
-        dev.load = function (file, successHandler) {
+    /**
+    * Loads a JavaScript file
+    *
+    * @param fileName {String}
+    * @param successHandler {Function}
+    */
+    function load(fileName, successHandler) {
 
-            // var rnd = Math.random();
-            // $.getScript(file + "?rnd=" + rnd, function (data, status) {
-            //     console.log(file + " is successfully loaded.");
-            //     if (successHandler !== undefined) {
-            //         successHandler();
-            //     }
-            // });
-            //
-            // return file + " is loading.";
-            return "Not implemented yet";
-        };
-
-        dev.pwd = function () {
-            return current;
-        };
-
-        dev.scripts = function () {
-
-            var result = [],
-                scripts = document.getElementsByTagName('script');
-
-            for (var i = 0; i < scripts.length; i++)  {
-                result.push(scripts[i]);
-            }
-
-            return result;
-        };
-
-        dev.rm = function (element) {
-
-            // var selected;
-            //
-            // if (current !== undefined) {
-            //     selected = $(element, current)[0];
-            //     if (selected !== undefined) {
-            //         return ($(selected).remove());
-            //     } else {
-            //         return element + " not found.";
-            //     }
-            // }
-            return "Not implemented yet";
-
-        };
+        // var rnd = Math.random();
+        // $.getScript(file + "?rnd=" + rnd, function (data, status) {
+        //     console.log(file + " is successfully loaded.");
+        //     if (successHandler !== undefined) {
+        //         successHandler();
+        //     }
+        // });
+        //
+        // return file + " is loading.";
+        return "Not implemented yet";
+    }
 
 
-        dev.csss = function () {
+    /**
+    * Return the current node
+    *
+    * @return {HtmlNode}
+    */
+    function pwd() {
 
-            // var result = [];
-            // $("link[rel=stylesheet]").each( function iterate(index, element) {
-            //     result.push($(element).attr("href"));
-            // });
-            //
-            // return result;
-            return "Not implemented yet";
-        };
+        return current;
+    }
 
-        // var target = console.__proto__;
-        var target = window;
+    /**
+    * Shows all the script elements found in the DOM
+    *
+    * @return {Array}
+    */
+    function scripts() {
 
-        ([["cd", cd],
-          ["cat", dev.cat],
-          ["load", dev.load],
-          ["rm", dev.rm]
-          // ["_reload", dev._reloadConsole]
-        ]).forEach( function iterate(element, index, array) {
-            if (target[element[0]] === undefined) {
-                target[element[0]] = element[1];
-            }
-        });
+        var result = [],
+            scripts = document.getElementsByTagName('script');
 
-        ([["ls", ls],
-          ["pwd", dev.pwd],
-          ["scripts", dev.scripts],
-          ["csss", dev.csss]
-        ]).forEach( function iterate(element, index, array) {
-            if (target[element[0]] === undefined) {
-                target.__defineGetter__(element[0], element[1]);
-            }
-        });
+        for (var i = 0; i < scripts.length; i++)  {
+            result.push(scripts[i]);
+        }
+
+        return result;
+    }
+
+    /**
+    * Removes the first childnode that matches the nodeDescription
+    *
+    * @param nodeDescription {String}
+    */
+    function rm(nodeDescription) {
+
+        // var selected;
+        //
+        // if (current !== undefined) {
+        //     selected = $(element, current)[0];
+        //     if (selected !== undefined) {
+        //         return ($(selected).remove());
+        //     } else {
+        //         return element + " not found.";
+        //     }
+        // }
+        return "Not implemented yet";
+
+    }
+
+    /*
+    * Shows all the script elements found in the DOM
+    */
+    function csss() {
+
+        // var result = [];
+        // $("link[rel=stylesheet]").each( function iterate(index, element) {
+        //     result.push($(element).attr("href"));
+        // });
+        //
+        // return result;
+        return "Not implemented yet";
+    }
+
+    /*
+    * Noconflict
+    */
+    function setNoConflictMode() {
 
 
-        // constants, this should be refactored later
-        (["html","head", "body", "script", "link", "div","h1","h2","h3","h4","p","strong","span","a"]).forEach( function iterate(element, index, array) {
-            // we overwrite everything, yeah
-            target[element] = element;
-        });
+    }
+
+    // var target = console.__proto__;
+    var target = window;
+
+    ([["cd", cd],
+      ["cat", cat],
+      ["load", load],
+      ["rm", rm]
+      // ["_reload", dev._reloadConsole]
+    ]).forEach( function iterate(element, index, array) {
+        if (target[element[0]] === undefined) {
+            target[element[0]] = element[1];
+        }
+    });
+
+    ([["ls", ls],
+      ["pwd", pwd],
+      ["scripts", scripts],
+      ["csss", csss]
+    ]).forEach( function iterate(element, index, array) {
+        if (target[element[0]] === undefined) {
+            target.__defineGetter__(element[0], element[1]);
+        }
+    });
 
 
-        // goes to the document
-        current = document;
+    // constants, this should be refactored later
+    (["html","head", "body", "script", "link", "div","h1","h2","h3","h4","p","strong","span","a"]).forEach( function iterate(element, index, array) {
+        // we overwrite everything, yeah
+        target[element] = element;
+    });
+
+
+    // goes to the document
+    current = document;
 };
 
 // inserting it to the DOM
