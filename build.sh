7599e8e85159abc7cd2e7538d79b9dbdc7999bc0
Viktor Kelemen <viktor@cirius.co.jp>
2010-08-31 22:19:26 +0900

    Build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Makes it ready to be uploaded
+
+cd ./src
+
+# version number of the extension
+version=`eval cat manifest.json | grep "version" | cut -d \" -f 4`
+
+# compressing
+zip chrome-console_$version.zip *
+mv chrome-console_$version.zip ../bin
+cd ..
